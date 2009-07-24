0272886dc66e34f9c57aa10dfd6a8c5468aa58a5
John Miller <jfmiller28@laptop.(none)>
2009-07-24 15:18:53 -0700

    Named instances and Updater.for

--- lib/updater.rb
+++ lib/updater.rb
@@ -4,17 +4,22 @@ require 'dm-core'
 require 'dm-types'
 
 class Updater
-  VERSION = '0.1.0'
+  attr_reader :error
+  VERSION = File.read(File.join(File.dirname(__FILE__),'..','VERSION')).strip
 
   include DataMapper::Resource
   
   property :id, Serial
   property :target, Class
-  property :ident, Object
+  property :ident, Yaml
   property :method, String
   property :finder, String
   property :args, Object
   property :time, Integer
+  property :name, String
+  
+  #will be called if an error occurs
+  belongs_to :failure, :class_name=>'Updater', :child_key=>[:failure_id]
   
   def target
     return @target if @ident.nil?
@@ -26,6 +31,8 @@ class Updater
     begin
       t.send(@method.to_sym,*@args)
     rescue => e
+      @error = e
+      failure.run if failure
       false
     end
     true
@@ -33,16 +40,31 @@ class Updater
   
   class << self
     def at(time,target,method,args,options={})
+      finder, finder_args = [:finder,:finder_args].map {|key| options.delete(key)}
       hash = {:method=>method.to_s,:args=>args}
       hash[:target] = target_for(target)
-      hash[:ident] = ident_for(target,options[:finder],options[:finder_args])
-      hash[:finder] = options[:finder] || :get
+      hash[:ident] = ident_for(target,finder,finder_args)
+      hash[:finder] = finder || :get
       hash[:time] = time
-      create(hash)
+      create(hash.merge(options))
     end
-    
+        
     def immidiate(*args)
       at(time.now,*args)
+    end
+    
+    def chain(*args)
+      at(nil,*args)
+    end
+    
+    def for(target,name=nil)
+      ident = ident_for(target)
+      target = target_for(target)
+      if name
+        first(:target=>target,:ident=>ident,:name=>name)
+      else
+        all(:target=>target,:ident=>ident)
+      end
     end
     
     def time
@@ -61,6 +83,8 @@ class Updater
       all(:time.gt=>time.now.to_i)
     end
     
+    
+    
   private
     
     def target_for(inst)
@@ -68,7 +92,7 @@ class Updater
       inst.class
     end
     
-    def ident_for(target,finder,args)
+    def ident_for(target,finder=nil,args=nil)
       if !(target.kind_of?(Class)) || finder
         return target.id unless finder
         args
@@ -78,4 +102,8 @@ class Updater
   
   end
   
+  
+  def inspect
+    "#<Updater id=#{id} target=#{target.inspect}>"
+  end
 end
--- spec/update_spec.rb
+++ spec/update_spec.rb
@@ -24,12 +24,17 @@ describe Updater do
     DataMapper::Resource.descendants.to_a.should include(Updater)
   end
   
+  it "should have a version matching the VERSION file" do
+    Updater::VERSION.should == File.read(File.join(ROOT,'VERSION')).strip
+  end
+  
   describe "adding an immidiate update request" do
     
     it "with a class target" do
       u = Updater.immidiate(Foo,:bar,[])
       u.target.should == Foo
       Updater.current.should include(u)
+      Updater.delayed.should_not include(u)
     end
     
     it "with an conforming instance target" do
@@ -37,6 +42,7 @@ describe Updater do
       u = Updater.immidiate(f,:bar,[])
       u.target.should == f
       Updater.current.should include(u)
+      Updater.delayed.should_not include(u)
     end
     
     it "with an custome finder" do
@@ -44,6 +50,35 @@ describe Updater do
       u = Updater.immidiate(Foo,:bar,[],:finder=>:first, :finder_args=>{:name=>'baz'})
       u.target.should == f
       Updater.current.should include(u)
+      Updater.delayed.should_not include(u)
+    end
+    
+  end
+  
+  describe "chained request" do
+    
+    it "should not be in current or delayed queue" do
+      u = Updater.chain(Foo,:bar,[:error])
+      u.time.should be_nil
+      Updater.current.should_not include(u)
+      Updater.delayed.should_not include(u)
+    end
+    
+  end
+  
+  describe "named request" do
+    
+    it "should be found by name when instance" do
+      f = Foo.create(:name=>'Honey')
+      u = Updater.immidiate(f,:bar,[:named],:name=>'Now')
+      u.name.should ==("Now")
+      Updater.for(f, "Now").should ==(u)
+    end
+    
+    it "should be found by name when class" do
+      u = Updater.immidiate(Foo,:bar,[:named],:name=>'Now')
+      u.name.should ==("Now")
+      Updater.for(Foo, "Now").should ==(u)
     end
     
   end
@@ -90,11 +125,25 @@ describe Updater do
       u.run
     end
     
+    it "should return all updates for a given target" do
+      u1 = Updater.immidiate(Foo,:bar,[:arg1,:arg2])
+      u2 = Updater.immidiate(Foo,:bar,[:arg3,:arg4])
+      Updater.for(Foo).should include(u1,u2)
+    end
+    
     describe "Error Handeling" do
       it "should trap errors" do
         u = Updater.immidiate(Foo,:bar,[:arg1,:arg2])
         Foo.should_receive(:bar).with(:arg1,:arg2).and_raise(RuntimeError)
         lambda {u.run}.should_not raise_error
+      end
+      
+      it "should run the failure task" do
+        err = Updater.chain(Foo,:bar,[:error])
+        u = Updater.immidiate(Foo,:bar,[:arg1,:arg2],:failure=>err)
+        Foo.should_receive(:bar).with(:arg1,:arg2).and_raise(RuntimeError)
+        Foo.should_receive(:bar).with(:error)
+        u.run
       end
     end
   end
